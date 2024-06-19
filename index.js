eba9de035c3550a65ca4d02d24589c60ace86232
N4meN1ck <kuzmaskill@gmail.com>
2024-06-19 20:40:00 +0100

    Connect static files

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 const app = express();
 const port = 3000;
+app.use(express.static("public"));
 app.listen(port, () =>{
     console.log(`Running on port ${port}`);
 });
