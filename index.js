bdb9ee16bc3bf9077c76207356b1798206f6a5a8
N4meN1ck <kuzmaskill@gmail.com>
2024-06-19 20:38:20 +0100

    Initialise the express application

---
+++ index.js
@@ -0,0 +1,10 @@
+import express from "express";
+const app = express();
+const port = 3000;
+app.listen(port, () =>{
+    console.log(`Running on port ${port}`);
+});
+// The landing page on which we can put documentation about the API.
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+});
